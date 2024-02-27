1b4bdcf756f709818e81d3e71ebb3353e98373be
Paul John Worrall <157617121+masterworrall@users.noreply.github.com>
2024-02-27 09:44:18 +0000

    Basic skeleton for help call

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Modal } from 'react-native';
+import bclogo from './assets/bclogo.webp'
+
+import SupportCircle from './components/SupportCircle.js'
 
 export default function App() {
+  const [ isModalVisible, setIsModalVisible ] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={bclogo} style={{width: 100, height: 100}}></Image>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+
+      <Button title="HELP!" onPress={() => setIsModalVisible(true)} />
+
+      <Modal visible={isModalVisible} onRequestClose={ () => setIsModalVisible(false)} 
+        animationType="slide" color="#247197">
+        <View style={styles.modal}>
+          <SupportCircle/>
+          <Button title="Close" onPress={() => setIsModalVisible(false)} color='#22c096'/>
+        </View>
+      </Modal >
+
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +30,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#247212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modal: {
+    flex: 1,
+    backgroundColor: '#22c096',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
---
+++ components/SupportCircle.js
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native'
+
+export default function Circle() {
+    return (
+
+        <View>
+            <Text>Here is your support circle!</Text>
+        </View>
+
+    )
+}
